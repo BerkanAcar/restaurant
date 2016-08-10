03571f566964a449d98eef5b16c478f8ef08189f
BerkanAcar <berkanacar93@gmail.com>
2016-08-10 09:33:15 +0300

    add show method in places controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'places', to: 'places#index'
+  get 'places/:id', to: 'places#show'
 end
