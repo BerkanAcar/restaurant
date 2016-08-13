2101958eeddaa88c801b4ca3a9c9893654ecf170
BerkanAcar <berkanacar93@gmail.com>
2016-08-13 12:24:16 +0300

    add foreign key migration to places

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160813085938) do
+ActiveRecord::Schema.define(version: 20160813091615) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,4 +34,5 @@ ActiveRecord::Schema.define(version: 20160813085938) do
     t.datetime "updated_at",     null: false
   end
 
+  add_foreign_key "places", "categories"
 end
