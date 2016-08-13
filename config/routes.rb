8a1bd63c113a45db0ccea93b60966eab3caa4f2b
BerkanAcar <berkanacar93@gmail.com>
2016-08-13 20:55:58 +0300

    add link for views and update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  #root: 'places'
-  get 'places', to: 'places#index'
-  get 'places/new', to: 'places#new'
-  post 'places', to: 'places#create'
-  get 'places/:id', to: 'places#show', as: 'place'
-  get 'places/:id/edit', to: 'places#edit'
-  patch 'places/:id', to: 'places#update'
-  delete 'places/:id', to: 'places#destroy', as: 'delete_place'
+  root 'places#index'
+  resources :places
 end
